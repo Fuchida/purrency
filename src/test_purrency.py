c5a76567060b05580b150124a180fa7fd5344484
Abdul Salim <abdlsalim@gmail.com>
2017-03-02 11:12:07 -0600

    Add source code

---
+++ src/test_purrency.py
@@ -0,0 +1,23 @@
+"""
+Tests for the parser
+"""
+
+from unittest import TestCase
+from purrency import Purrency
+from decimal import Decimal
+
+
+class TestPurrency(TestCase):
+
+    def setUp(self):
+        self.expected_test_dollar = {'amount': Decimal(200.0), 'currency': '$'}
+        test_dollar = '$200.00'
+
+        self.purr = Purrency(test_dollar)
+        self.parsed_dollar = self.purr.parse()
+
+    def test_dollar(self):
+        """
+        Parse USD
+        """
+        self.assertEqual(self.expected_test_dollar, self.parsed_dollar)
